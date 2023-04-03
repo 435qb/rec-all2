720341edf5ae63c0af1030a6d3b5370b5ff85937
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-04-03 22:10:48 +0800

    NA

--- Core/List/List.Api/Controllers/ListController.cs
+++ Core/List/List.Api/Controllers/ListController.cs
@@ -12,6 +12,15 @@ public class ListController {
     private readonly IMediator _mediator;
     private readonly IIdentityService _identityService;
     private readonly ILogger<ListController> _logger;
+    
+    public ListController(IMediator mediator, IIdentityService identityService,
+        ILogger<ListController> logger) {
+        _mediator = mediator ??
+            throw new ArgumentNullException(nameof(mediator));
+        _identityService = identityService ??
+            throw new ArgumentNullException(nameof(identityService));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
 
     [Route("create")]
     [HttpPost]
